3fe9210df74ce199ff98886a5b04ab513907f1ea
realingy <754037927@qq.com>
2019-04-01 10:51:48 +0800

    add file

---
+++ src/Dll1/Dll1.cpp
@@ -0,0 +1,5 @@
+#include "Dll1.h"
+
+int FuncMultiply(int x, int y) {
+	return x * y;
+}
---
+++ src/StartUseDll/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//#include "../Dll1/Dll1.h"
+#include "Dll1.h"
+
+int main() {
+	std::cout << "Result Of Function Multiply(13,3) = " << FuncMultiply(13, 3) << std::endl;
+ 	return 0;
+}
