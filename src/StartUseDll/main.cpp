b49b7d777196789436c401f16425339511443556
realingy <754037927@qq.com>
2019-04-01 13:31:04 +0800

    edit dll cmakelists

--- src/StartUseDll/main.cpp
+++ src/StartUseDll/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-//#include "../Dll1/Dll1.h"
 #include "Dll1.h"
 
 int main() {
-	std::cout << "Result Of Function Multiply(13,3) = " << FuncMultiply(13, 3) << std::endl;
+	int x, y;
+	std::cin >> x;
+	std::cin >> y;
+	std::cout << "Result Of Function Multiply(" << x << ", " << y << ") = " << FuncMultiply(x, y) << std::endl;
  	return 0;
 }
